956ec34a2463a00eb0c5fabdff08427bee9d28e9
marina-nurce <marinanurce25@gmail.com>
2024-04-25 11:58:58 +0200

    Projekti  i perfunduar

---
+++ Library.API/Services/IBooksService.cs
@@ -0,0 +1,13 @@
+﻿using Library.API.Dto;
+
+namespace Library.API.Services
+{
+    public interface IBooksService
+    {
+        List<Models.Books> GetAllBooks();
+        Models.Books GetBooksById(int id);
+        Models.Books AddBooks(PostLibraryDto books);
+        Models.Books UpdateBooks(PutLibraryDto booksData, int id);
+        void DeleteBooks(int id);
+    }
+}
